66b607141a69ea37a51a527bafb031a17b895efd
MinhNguyen <3langn@gmail.com>
2021-10-12 11:46:28 +0700

    done

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
+import puppeteer from "puppeteer";
 import fs from "fs";
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://en.wikipedia.org/wiki/Node.js", {
+    waitUntil: "networkidle2",
+  });
+  const data = await page.$eval("*", (el) => {
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNode(el);
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+    return window.getSelection()?.toString();
+  });
+  console.log(data);
 
-let [, , filepath] = process.argv;
-filepath += ".txt";
-fs.writeFile(filepath, "Something...", (err) => {
-  if (err) console.log(err);
-  const file = fs.readFileSync(filepath, "utf8");
-  console.log(file);
-});
+  fs.writeFileSync("content.txt", data!);
+  await browser.close();
+})();
