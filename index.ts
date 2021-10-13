76ac7c8eda112d8b54ee7131ec33f82e2d3fb843
MinhNguyen <3langn@gmail.com>
2021-10-13 18:27:10 +0700

    add winston

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import express from "express";
-import readData from "./middleware/read-data";
-import router from "./routes";
-import passport from "passport";
-import jwtStrategy from "./config/passport";
+import express from 'express';
+import readData from './middleware/read-data';
+import router from './routes';
+import passport from 'passport';
+import jwtStrategy from './config/passport';
+import logger from './config/logger';
 const app = express();
 
 app.use(express.json());
@@ -13,5 +14,5 @@ passport.use(jwtStrategy);
 app.use(router);
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  logger.info('Server listening on port 3000');
 });
